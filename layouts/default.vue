7fc1628b414b25ed2f89aaf4ca1f6298ec882b10
=elirehema <=rocksandystone@gmail.com>
2020-09-20 17:54:08 +0300

    Added Navigation Drawer with menus

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,36 @@
 <template>
   <v-app class="app">
     <!--NAVIGATION DRAWER-->
-    <v-navigation-drawer class="primary" elevation="0" v-model="drawer" width="180" app>
-      <v-toolbar color="primary" elevation="0">
-        <v-spacer></v-spacer>
+    <v-navigation-drawer 
+     v-model="drawer"
+      :mini-variant="$vuetify.breakpoint.mdAndDown"
+      :expand-on-hover="$vuetify.breakpoint.mdAndDown"
+      overlay-color="primary"
+      color="primary"
+      clipped
+      :absolute="$vuetify.breakpoint.mdAndUp"
+    fixed app
+     width="180">
 
-        <v-toolbar-items></v-toolbar-items>
-      </v-toolbar>
-
-      <v-list nav dense class="mt-12 pa-0">
-        <v-list-item-group>
-          <v-list-item
-            v-for="(item, i) in items"
+      <v-list nav dense subheader tile class="mt-0 pa-0">
+         <v-divider light ></v-divider>
+        <template v-for="(item, i) in items">
+          <v-list-item class="ma-0" dense
             :key="`${i}-${item.route}`"
-            v-on:click="nativateToHere(item.route)"
-          >
-            <v-list-item-icon>
-              <v-icon color="default" v-text="item.icon"></v-icon>
-            </v-list-item-icon>&nbsp;&nbsp;
-            <v-list-item-content>
-              <v-list-item-title class="font-weight-bold default--text" v-text="item.text"></v-list-item-title>
+            v-on:click="nativateToHere(item.route)">
+            <v-list-item-icon class="mr-0">
+              <v-icon color="default" v-text="item.icon" small></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content class="ma-0 pa-0">
+              <v-list-item-title class="font-weight-light default--text" v-text="item.text"></v-list-item-title>
             </v-list-item-content>
           </v-list-item>
-        </v-list-item-group>
+
+        <v-divider light
+          :key="i"
+          :inset="item.inset"
+        ></v-divider>
+        </template>
       </v-list>
       <template v-slot:append>
         <v-list-item>
@@ -35,26 +43,20 @@
         </v-list-item>
       </template>
     </v-navigation-drawer>
-    <v-app-bar elevation="0" :clipped-left="clipped" color="primary" fixed app dark>
-      <v-toolbar-title>
-        <v-avatar color="primary lighten-1" size="36">
-          <span class="white--text font-weight-bold overline" @click.stop="drawer = !drawer">
-            <v-icon small color="white">mdi-dialpad</v-icon>
-            <!--<v-app-bar-nav-icon color="white" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>-->
-          </span>
-        </v-avatar>
-        <span
-          class="hidden-sm-and-down font-weight-bold default--text"
-        >Welcome, {{ userdata.username }}</span>
-      </v-toolbar-title>
+    <v-app-bar
+    absolute
+    clipped-left
+    fixed app>
+      <v-toolbar-side-icon >
+        <img width="30%" src="../assets/images/logo.png" alt="Header Image"/>
+      </v-toolbar-side-icon>
 
       <v-spacer></v-spacer>
-      <span class="default--text font-weight-bold hidden-sm-and-down">{{todayDate}}</span>
       <v-tooltip bottom color="primary" open-on-hover open-delay="500">
         <template v-slot:activator="{ on }">
-          <v-btn icon v-on="on" @click.stop="syncro()">
+          <v-btn icon v-on="on" @click.stop="syncro()" x-small>
             <v-progress-circular v-if="sync" v-on="on" size="20" indeterminate color="white"></v-progress-circular>
-            <v-icon v-else color="default">mdi-sync</v-icon>
+            <v-icon v-else small >mdi-sync</v-icon>
           </v-btn>
         </template>
 
@@ -70,16 +72,16 @@
         open-delay="500"
       >
         <template v-slot:activator="{ on }">
-          <v-btn icon v-on="on">
-            <v-icon color="default">mdi-cog</v-icon>
+          <v-btn icon v-on="on" x-small>
+            <v-icon small>mdi-cog</v-icon>
           </v-btn>
         </template>
         <span>Settings Configuration</span>
       </v-tooltip>
       <v-tooltip bottom color="primary" open-on-hover open-delay="500">
         <template v-slot:activator="{ on }">
-          <v-btn icon v-on="on">
-            <v-icon color="default" @click="logoutsession">mdi-logout-variant</v-icon>
+          <v-btn icon v-on="on" x-small>
+            <v-icon small  @click="logoutsession">mdi-logout-variant</v-icon>
           </v-btn>
         </template>
         <span>Click to logout</span>
@@ -114,7 +116,7 @@ export default {
     "sectors-component": SectorsComponent,
     "indicatorsource-component": IndicatorSourceComponent,
     "currentrelease-component": CurrentReleasesComponent,
-    "disaggregations-component": DisaggregationsComponent
+    "disaggregations-component": DisaggregationsComponent,
   },
   data() {
     return {
@@ -132,43 +134,97 @@ export default {
         { title: "Sectors", icon: "mdi-eye" },
         { title: "Indicator Source", icon: "mdi-eye" },
         { title: "Current Release", icon: "mdi-eye" },
-        { title: "Disaggregations", icon: "mdi-eye" }
+        { title: "Disaggregations", icon: "mdi-eye" },
       ],
       titles: {
-        title: "Ospic"
+        title: "Ospic",
       },
 
       items: [
         {
           text: "Dashboard",
-          icon: "mdi-view-dashboard",
-          route: "dashboard",
-          subtitle: "Lorem ipsum dolor sit de amet.."
+          icon: "mdi-hospital-building",
+          route: "",
+          subtitle: "Lorem ipsum dolor sit de amet..",
         },
         {
-          text: "Home",
-          icon: "mdi-home-variant",
+          text: "Patient Registration",
+          icon: "mdi-bunk-bed-outline",
           route: "home",
-          subtitle: "Lorem ipsum dolor sit de amet.."
+          subtitle: "Lorem ipsum dolor sit de amet..",
         },
         {
-          text: "Documents",
-          icon: "mdi-file",
+          text: "Appointments",
+          icon: "mdi-file-cabinet",
           route: "home",
-          subtitle: "Lorem ipsum dolor sit de amet ..."
+          subtitle: "Lorem ipsum dolor sit de amet ...",
         },
-        {
-          text: "Help",
-          icon: "mdi-help-circle",
-          route: "home",
-          subtitle: "Lorem ipsum dolor sit de amet ..."
+         {
+          text: "OPD Management",
+          icon: "mdi-zodiac-cancer",
+          route: "",
+          subtitle: "Lorem ipsum dolor sit de amet..",
+        },
+         {
+          text: "Laboratoty",
+          icon: "mdi-beaker-question-outline",
+          route: "",
+          subtitle: "Lorem ipsum dolor sit de amet..",
+        },
+         {
+          text: "Radiology",
+          icon: "mdi-radioactive",
+          route: "",
+          subtitle: "Lorem ipsum dolor sit de amet..",
+        },
+         {
+          text: "In-Patient Management(ATD)",
+          icon: "mdi-home-roof",
+          route: "",
+          subtitle: "Lorem ipsum dolor sit de amet..",
+        },
+          {
+          text: "In-Patient Billing",
+          icon: "mdi-credit-card-wireless",
+          route: "",
+          subtitle: "Lorem ipsum dolor sit de amet..",
+        },
+          {
+          text: "Nurse Station",
+          icon: "mdi-zodiac-cancer",
+          route: "",
+          subtitle: "Lorem ipsum dolor sit de amet..",
+        },
+          {
+          text: "Pharmacy",
+          icon: "mdi-pharmacy",
+          route: "",
+          subtitle: "Lorem ipsum dolor sit de amet..",
+        },
+          {
+          text: "Mis Reports",
+          icon: "mdi-folder-text-outline",
+          route: "",
+          subtitle: "Lorem ipsum dolor sit de amet..",
+        },
+          {
+          text: "Software Management (Setting)",
+          icon: "mdi-database-settings",
+          route: "",
+          subtitle: "Lorem ipsum dolor sit de amet..",
+        },
+          {
+          text: "User Management",
+          icon: "mdi-account-cog-outline",
+          route: "",
+          subtitle: "Lorem ipsum dolor sit de amet..",
         }
       ],
       actions: [
         // { title: "View profile", icon: "mdi-account-circle" },
         { title: "Settings", icon: "mdi-cog" },
         { title: "Help", icon: "mdi-help" },
-        { title: "Logout", icon: "mdi-logout-variant" }
+        { title: "Logout", icon: "mdi-logout-variant" },
       ],
       miniVariant: false,
       right: true,
@@ -176,14 +232,14 @@ export default {
       collapseOnScroll: true,
       rightDrawer: false,
       year: new Date().getFullYear(),
-      userdata:{
-        username: 'Elirehema Paul'
-      }
+      userdata: {
+        username: "Elirehema Paul",
+      },
     };
   },
 
   methods: {
-    selectedItemAction: function(item) {
+    selectedItemAction: function (item) {
       switch (item) {
         case 0:
           //this.$router.push("/profile");
@@ -196,10 +252,10 @@ export default {
           break;
       }
     },
-    logoutsession: function() {
+    logoutsession: function () {
       this.$store.dispatch("logout");
     },
-    changemode: function() {
+    changemode: function () {
       this.dark = !this.dark;
       this.$vuetify.theme.dark = this.dark;
     },
@@ -213,75 +269,9 @@ export default {
         this.$vuetify.theme.dark = false;
       }
     },
-      },
-  beforeMount: function() {
-    
   },
-  computed: {
-  
-    todayDate() {
-      var objToday = new Date(),
-        weekday = new Array(
-          "Sunday",
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday"
-        ),
-        dayOfWeek = weekday[objToday.getDay()],
-        domEnder = (function() {
-          var a = objToday;
-          if (/1/.test(parseInt((a + "").charAt(0)))) return "th";
-          a = parseInt((a + "").charAt(1));
-          return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th";
-        })(),
-        dayOfMonth =
-          today + (objToday.getDate() < 10)
-            ? "0" + objToday.getDate() + domEnder
-            : objToday.getDate() + domEnder,
-        months = new Array(
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December"
-        ),
-        curMonth = months[objToday.getMonth()],
-        curYear = objToday.getFullYear(),
-        curMinute =
-          objToday.getMinutes() < 10
-            ? "0" + objToday.getMinutes()
-            : objToday.getMinutes(),
-        curSeconds =
-          objToday.getSeconds() < 10
-            ? "0" + objToday.getSeconds()
-            : objToday.getSeconds(),
-        curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
-      var today =
-        dayOfWeek +
-        " " +
-        dayOfMonth +
-        " of " +
-        curMonth +
-        ", " +
-        curYear +
-        curMinute +
-        "." +
-        curSeconds +
-        curMeridiem +
-        " ";
-      return today;
-    }
-  }
+  beforeMount: function () {},
+  computed: { },
 };
 </script>
 <style>
