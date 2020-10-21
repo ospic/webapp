cbd6661ceb406b2f1d5660bf6c52f1d947af9644
=elirehema <=rocksandystone@gmail.com>
2020-10-21 22:28:35 +0300

    Chnage Hid assets to file

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Open source Hospital Management System' },
+     // { hid: 'description', name: 'description', content: 'Open source Hospital Management System' },
       { hid: 'og:image', property: 'og:image', content:  ogImage }
    
     ],
