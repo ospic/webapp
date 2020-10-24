2aa626d1b30014819a98814a827e0d1617878a9d
=elirehema <=rocksandystone@gmail.com>
2020-10-24 21:39:19 +0300

    Eleveted cards

--- components/charts/summary-card.vue
+++ components/charts/summary-card.vue
@@ -1,6 +1,6 @@
 <template>
   <v-col class="pa-1" cols="12" xs="4" sm="3" md="2" xl="1">
-    <v-card class="mx-auto" elevation="1" outlined>
+    <v-card class="mx-auto" color="white" elevation="1">
       <v-card-title class="font-weight-bold caption py-2   ma-0 pr-1 "
         >{{ data.title }}<v-spacer></v-spacer
         ><v-btn
@@ -14,27 +14,19 @@
         ></v-card-title
       >
       <v-divider></v-divider>
-      <v-list-item three-line dense>
-        <v-list-item-content class="ma-0 pa-0">
+      <v-list-item>
+        <v-list-item-content class="mt-1">
           <v-list-item-subtitle
-            class="font-weight-light light-blue--text text-h6 ma-0 pa-1"
-            >{{ data.value }}</v-list-item-subtitle
-          >
-          <v-list-item-subtitle
-            dense
-            class="font-weight-light caption  ma-0 pa-0"
-          >
+            class="font-weight-light light-blue--text text-subtitle-2"
+            >{{ data.value }}
+          </v-list-item-subtitle>
+          <v-list-item-subtitle class="font-weight-light caption  ma-0 pa-0">
             {{ data.subtitle }}
           </v-list-item-subtitle>
         </v-list-item-content>
-
-        <v-list-item-avatar
-          class="ma-0 mt-1 pa-0 "
-          size="30"
-          color="white lighten-2"
-        >
-          <v-icon dark v-text="data.icon" color="primary"></v-icon
-        ></v-list-item-avatar>
+        <v-list-item-icon>
+          <v-icon dark v-text="data.icon" color="primary" small></v-icon>
+        </v-list-item-icon>
       </v-list-item>
     </v-card>
   </v-col>
