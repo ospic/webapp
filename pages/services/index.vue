f05202d50c16f27abdd605f433de16a166550a04
=elirehema <=rocksandystone@gmail.com>
2021-01-24 16:59:19 +0300

    Fetch services from store

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,57 +1,31 @@
 <template>
-  <service-card :services="services"></service-card>
+  <div>
+    <div class="breadcrumb " v-if="services != null">
+      <router-link to="/">Dashboard</router-link>
+      <router-link to="/services" class="active">service </router-link>
+    </div>
+
+    <service-card :services="services"></service-card>
+  </div>
 </template>
 <script>
 import ServiceCard from "@/components/profile/card_service";
+import { mapGetters } from "vuex";
 export default {
   components: {
     "service-card": ServiceCard
   },
   data: function() {
-    return {
-      services: [],
-      search: null,
-      headers: [
-        { text: "ID", value: "id" },
-        { text: "Patient Name", value: "patientName", sortable: false },
-        { text: "Staff Name", value: "staffName" },
-        { text: "Is Active?", value: "isActive" },
-        { text: "From", value: "fromDate" },
-        { text: "To", value: "toDate" }
-      ],
-      body: {
-        options: {
-          page: 1,
-          itemsPerPage: 5,
-          sortBy: ["patientName"]
-        }
-      }
-    };
+    return {};
   },
-  methods: {
-    async getAllServices() {
-      return await this.$api
-        .$get(`services/`)
-        .then(response => {
-          if (response !== null) {
-            this.services = response.sort(function(a, b) {
-              return b.isActive - a.isActive;
-            });
-          }
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
-    handleClick: function(value) {
-      this.$router.push("/services/" + value.id);
-    },
-    updatePagination: function(val) {
-      console.log(val);
-    }
-  },
+  methods: {},
   created() {
-    this.getAllServices();
+    this.$store.dispatch("fetch_all_services");
+  },
+  computed: {
+    ...mapGetters({
+      services: "services"
+    })
   }
 };
 </script>
