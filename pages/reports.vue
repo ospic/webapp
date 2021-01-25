44a9484e408e209c467c232a1403ee8684f4a3f9
=elirehema <=rocksandystone@gmail.com>
2021-01-26 00:20:37 +0300

    Pdf report trial 1

--- pages/reports.vue
+++ pages/reports.vue
@@ -5,7 +5,42 @@
       <router-link to="/reports" class="active">Reports</router-link>
     </div>
     <v-card class="pa-3">
-      <h1>ALl Reports will be Displayed Here</h1>
+      <h1>Example of reports (Under development)</h1>
+      <div name="link" id="link"></div>
+      <div>
+        <pdf :src="response_url"></pdf>
+      </div>
     </v-card>
   </div>
 </template>
+<script>
+import pdf from "vue-pdf";
+export default {
+  components: {
+    pdf
+  },
+  data: function() {
+    return {
+      response_url: null,
+      response_data: null
+    };
+  },
+  methods: {
+    async load_pdf() {
+      return await this.$api
+        .$get(`test/view`)
+        .then(response => {
+          this.response_data = response;
+          this.response_url = this.$api.defaults.baseURL + "test/view";
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+
+  created() {
+    this.load_pdf();
+  }
+};
+</script>
