2eea2c35e0a7d96df999eca55bda148446327b46
=elirehema <=rocksandystone@gmail.com>
2021-02-11 18:22:47 +0300

    theme imported mutations

--- store/theme.js
+++ store/theme.js
@@ -1,25 +1,24 @@
-import * as mutation from './mutation-types';
 const state = () => ({
   isDark: false
 });
 
 const mutations = {
-  [mutation.LIGHT_THEME](state) {
+  ["LIGHT_THEME"](state) {
     state.isDark = false;
   },
 
-  [mutation.DARK_THEME](state) {
+  ["DARK_THEME"](state) {
     state.isDark = true;
   },
 
 }
 const actions = {
   async change_to_dark({ commit }) {
-    commit(mutation.DARK_THEME);
+    commit("DARK_THEME");
 
   },
   async change_to_light({ commit }) {
-    commit(mutation.LIGHT_THEME);
+    commit("LIGHT_THEME");
   }
 }
 const getters = {
