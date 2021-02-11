3cb7e4897eec1c556f4aeb02c9eada360d00f583
=elirehema <=rocksandystone@gmail.com>
2021-02-11 18:19:10 +0300

    remove imported mutations

--- store/documents.js
+++ store/documents.js
@@ -1,4 +1,3 @@
-import * as mutation from './mutation-types';
 const state = () => ({
   showLoader: Boolean,
   image: {},
@@ -7,31 +6,30 @@ const state = () => ({
 
 const mutations = {
 
-  [mutation.UPDATE_PATIENT_PROFILE_PICTURE](state) {
+  ["UPDATE_PROFILE_PICTURE"](state) {
     state.showLoader = true;
   },
-  [mutation.UPDATE_PATIENT_PROFILE_PICTURE_FAILED](state) {
+  ["UPDATE_PROFILE_PICTURE_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.UPDATE_PATIENT_PROFILE_PICTURE_ERROR](state) {
+  ["UPDATE_PROFILE_PICTURE_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.UPDATE_PATIENT_PROFILE_PICTURE_SUCCESS](state, payload) {
+  ["UPDATE_PROFILE_PICTURE_SUCCESS"](state, payload) {
     state.showLoader = false;
     this.image = payload;
   },
 
 }
 const actions = {
- 
   async update_patient_profile_image({ commit }, id, payload) {
-    commit(mutation.UPDATE_PATIENT_PROFILE_PICTURE);
+    commit("UPDATE_PROFILE_PICTURE");
     await this.$api.$patch(`patients/${id}/images/`, payload, con)
       .then(response => {
-        commit(mutation.UPDATE_PATIENT_PROFILE_PICTURE_SUCCESS, response);
+        commit("UPDATE_PROFILE_PICTURE_SUCCESS", response);
 
       }).catch(error => {
-        commit(mutation.UPDATE_PATIENT_PROFILE_PICTURE_ERROR);
+        commit("UPDATE_PROFILE_PICTURE_ERROR");
         console.log(error);
 
       });
