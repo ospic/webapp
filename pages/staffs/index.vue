46e4c05b9331fc5ac19313ecb2bb0776914450cd
=elirehema <=rocksandystone@gmail.com>
2021-02-16 19:00:01 +0300

    List staff by department

--- pages/staffs/index.vue
+++ pages/staffs/index.vue
@@ -7,11 +7,84 @@
     <v-container fluid>
       <v-card class="py-2 pl-1" flat>
         <v-card-title>{{ $t("label.titles.staffs") }}</v-card-title>
-        <v-row no-gutters>
+        <!-- <v-row no-gutters>
           <v-col v-for="(staff, n) in staffs" :key="n" sm="3" md="2">
             <staff-card :staff="staff"></staff-card>
-          </v-col>
-        </v-row>
+          </v-col> </v-row
+        >-->
+        <v-card-text>
+          <v-tabs v-model="tab" background-color="primary" dark>
+            <v-tab
+              v-for="department in departments"
+              :key="department.id"
+              @click="fetchDepartmentStaffs(department.id, department.name)"
+            >
+              {{ department.name }}
+            </v-tab>
+          </v-tabs>
+
+          <div class="mt-3">
+            <h3 v-if="title != null">{{ title }} Department Staff's</h3>
+            <v-progress-linear
+              v-if="updating"
+              indeterminate
+              color="cyan"
+              class="mb-1"
+            ></v-progress-linear>
+            <v-data-table
+              dense
+              v-else
+              :headers="headers"
+              :items="departmentsstaffs"
+              :search="search"
+              width="100%"
+              @click:row="handleClick"
+            >
+              <template v-slot:[`item.fullName`]="{ item }">
+                {{ item.fullName == null ? item.user.username : item.fullName }}
+              </template>
+              <template v-slot:[`item.department`]="{ item }">
+                {{ item.department.name }}
+              </template>
+              <template v-slot:[`item.email`]="{ item }">
+                {{ item.email === null ? item.user.email : item.email }}
+              </template>
+              <template v-slot:[`item.isActive`]="{ item }">
+                <v-tooltip right v-if="item.isActive" color="primary">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-icon v-bind="attrs" v-on="on" small color="red lighten-2"
+                      >mdi-check</v-icon
+                    >
+                  </template>
+                  <span>Active</span>
+                </v-tooltip>
+                <div v-else></div>
+              </template>
+              <template v-slot:[`item.isAvailable`]="{ item }">
+                <v-tooltip right v-if="item.isAvailable" color="primary">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-icon v-bind="attrs" v-on="on" small color="red lighten-2"
+                      >mdi-check</v-icon
+                    >
+                  </template>
+                  <span>Available</span>
+                </v-tooltip>
+                <div v-else></div>
+              </template>
+              <template v-slot:[`item.role`]="{ item }">
+                <v-chip
+                  small
+                  dense
+                  class="primary"
+                  v-for="role in item.user.roles"
+                  :key="role.id"
+                >
+                  {{ role.name.toLowerCase() }}
+                </v-chip>
+              </template>
+            </v-data-table>
+          </div>
+        </v-card-text>
       </v-card>
     </v-container>
   </div>
@@ -23,12 +96,57 @@ export default {
   components: {
     StaffCard
   },
+  data: () => ({
+    search: "",
+    updating: false,
+    headers: [
+      { text: "ID", value: "id" },
+      { text: "User/name", value: "fullName", sortable: false },
+      { text: "Department", value: "department" },
+      { text: "Email", value: "email" },
+      { text: "Phone", value: "contacts" },
+      { text: "Role", value: "role" },
+      { text: "Active ?", value: "isActive", sortable: true },
+      { text: "Available ?", value: "isAvailable", sortable: true }
+    ],
+    items: [
+      { title: "Edit", icon: "lead-pencil", color: "blue" },
+      { title: "Delete", icon: "delete", color: "red" }
+    ],
+    departmentsstaffs: [],
+    title: null
+  }),
+  methods: {
+    async fetchDepartmentStaffs(id, title) {
+      this.title = title;
+      this.updating = true;
+      await this.$api
+        .$get(`staffs/dep/${id}`)
+        .then(response => {
+          if (response != null) {
+            this.departmentsstaffs = response;
+            this.updating = false;
+          }
+        })
+        .catch(error => {
+          this.updating = false;
+          console.log(error);
+        });
+    },
+    handleClick(item) {
+      this.$router.push("staffs/" + item.id);
+    }
+  },
+  created() {
+    this.fetchDepartmentStaffs(5);
+  },
   beforeCreate() {
-    this.$store.dispatch("fetchAllStaffs");
+    //this.$store.dispatch("fetchAllStaffs");
   },
   computed: {
     ...mapGetters({
-      staffs: "staffs"
+      staffs: "staffs",
+      departments: "departments"
     })
   }
 };
