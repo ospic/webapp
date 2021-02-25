edf74d053caa880ac11c2894938eabdf60d0b0d8
=elirehema <=rocksandystone@gmail.com>
2021-02-25 16:20:29 +0300

    Increase bill receipt font size

--- pages/receipt.vue
+++ pages/receipt.vue
@@ -1,88 +1,32 @@
 <template>
-<v-container fluid>
-
-  <div id="invoice-POS" v-if="bill !== null">
-    
-    <center id="top">
-      <div class="logo"></div>
-      <div class="info"> 
-        <h2>Ospic Memorial Hospital</h2>
-      </div><!--End Info-->
-    </center><!--End InvoiceTop-->
-    
-    <div id="mid">
-      <div class="info">
-        <h2>Receipt Info</h2>
-           <p> 
-            Client : {{bill.patientName}}</br>
-            Email   : {{ bill.emailAddress }}</br>
-            Phone   : {{ bill.phoneNumber}} </br>
-            Address : {{bill.address}}
-        </p>
-      </div>
-    </div><!--End Invoice Mid-->
-    
-    <div id="bot">
-
-					<div id="table" v-if="transaction !== null">
-						<table>
-							<tr class="tabletitle">
-								<td class="item"><h2>Item</h2></td>
-								<td class="Hours"><h2>Medicine</h2></td>
-                	<td class="Hours"><h2>Service</h2></td>
-								<td class="Rate"><h2>Price</h2></td>
-							</tr>
-
-							<tr class="service" v-for="(trans, index) in transaction.transactions" :key="index">
-								<td class="tableitem"><p class="itemtext">Communication</p></td>
-								<td class="tableitem"><p class="itemtext">{{ trans.medicalServiceName === null ? '------': trans.medicalServiceName }}</p></td>
-								<td class="tableitem"><p class="itemtext">{{ trans.medicineName === null ? '------':trans.medicineName }}</p></td>
-							
-              	<td class="tableitem"><p class="itemtext">{{trans.amount}}</p></td>
-							</tr>
-
-						
-
-							<tr class="tabletitle">
-								<td></td><td></td>
-								<td class="Rate"><h2>Tax</h2></td>
-								<td class="payment"><h2>{{tax}}</h2></td>
-							</tr>
-
-							<tr class="tabletitle">
-								<td></td><td></td>
-								<td class="Rate"><h2>Total</h2></td>
-								<td class="payment"><h2>{{amountToPay}}</h2></td>
-							</tr>
-
-						</table>
-					</div><!--End Table-->
-
-					<div id="legalcopy">
-					<div >
-              <span class="stamp is-approved" v-if="!bill.isPaid"
-              >OutStanding !</span
-            >
-            <span class="stamp" style="height: 90px"  v-else>Paid </span>
-          </div>
-					</div>
-
-				</div><!--End InvoiceBot-->
-  </div><!--End Invoice-->
-
-  
-</v-container>
+  <v-container fluid>
+    <id>
+      <v-progress-linear
+        background-color="white"
+        indeterminate
+        color="cyan"
+        v-if="bill == null"
+      ></v-progress-linear>
+      <receipt v-else :bill="bill"></receipt>
+    </id>
+  </v-container>
 </template>
 <script>
+import ReceiptComponent from "@/components/finance/receipt_component.vue";
 export default {
-  data:()=>({
-      dialog: false,
-      bill: null,
-      transaction: null,
-      amountToPay: null,
-      tax: 0
+  layout: "finance",
+  components: {
+    receipt: ReceiptComponent
+  },
+
+  data: () => ({
+    dialog: false,
+    bill: null,
+    transaction: null,
+    amountToPay: null,
+    tax: 0
   }),
-  methods:{
+  methods: {
     async getbill() {
       return await this.$api
         .$get(`bills/1/`)
@@ -94,96 +38,115 @@ export default {
         .catch(error => {
           console.log(error);
         });
-    },
+    }
   },
-  created(){
-    this.getbill()
+  created() {
+    this.getbill();
   }
-}
+};
 </script>
 <style scoped lang="scss">
-#invoice-POS{
+#invoice-POS {
   box-shadow: 0 0 1in -0.25in rgba(0, 0, 0, 0.5);
-  padding:2mm;
+  padding: 2mm;
   margin: 0 auto;
   width: 80mm;
-  background: #FFF;
-  
-  
-::selection {background: #f31544; color: #FFF;}
-::moz-selection {background: #f31544; color: #FFF;}
-h1{
-  font-size: 1.5em;
-  color: #222;
-}
-h2{font-size: .9em;}
-h3{
-  font-size: 1.2em;
-  font-weight: 300;
-  line-height: 2em;
-}
-p{
-  font-size: .7em;
-  color: #666;
-  line-height: 1.2em;
-}
- 
-#top, #mid,#bot{ /* Targets all id with 'col-' */
-  border-bottom: 1px solid #EEE;
-}
+  background: #fff;
 
-#top{min-height: 100px;}
-#mid{min-height: 80px;} 
-#bot{ min-height: 50px;}
+  ::selection {
+    background: #f31544;
+    color: #fff;
+  }
+  ::moz-selection {
+    background: #f31544;
+    color: #fff;
+  }
+  h1 {
+    font-size: 1.5em;
+    color: #222;
+  }
+  h2 {
+    font-size: 0.9em;
+  }
+  h3 {
+    font-size: 1.2em;
+    font-weight: 300;
+    line-height: 2em;
+  }
+  p {
+    font-size: 0.7em;
+    color: #666;
+    line-height: 1.2em;
+  }
 
-#top .logo{
-  //float: left;
-	height: 60px;
-	width: 60px;
-	background: url(../static/icon.png) no-repeat;
-	background-size: 60px 60px;
-}
-.clientlogo{
-  float: left;
-	height: 60px;
-	width: 60px;
-	background: url(../static/logo.png) no-repeat;
-	background-size: 60px 60px;
-  border-radius: 50px;
-}
-.info{
-  display: block;
-  //float:left;
-  margin-left: 0;
-}
-.title{
-  float: right;
-}
-.title p{text-align: right;} 
-table{
-  width: 100%;
-  border-collapse: collapse;
-}
-td{
-  /*padding: 5px 0 5px 15px;
+  #top,
+  #mid,
+  #bot {
+    /* Targets all id with 'col-' */
+    border-bottom: 1px solid #eee;
+  }
+
+  #top {
+    min-height: 100px;
+  }
+  #mid {
+    min-height: 80px;
+  }
+  #bot {
+    min-height: 50px;
+  }
+
+  #top .logo {
+    //float: left;
+    height: 60px;
+    width: 60px;
+    background: url(../static/icon.png) no-repeat;
+    background-size: 60px 60px;
+  }
+  .clientlogo {
+    float: left;
+    height: 60px;
+    width: 60px;
+    background: url(../static/logo.png) no-repeat;
+    background-size: 60px 60px;
+    border-radius: 50px;
+  }
+  .info {
+    display: block;
+    //float:left;
+    margin-left: 0;
+  }
+  .title {
+    float: right;
+  }
+  .title p {
+    text-align: right;
+  }
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  td {
+    /*padding: 5px 0 5px 15px;
   border: 1px solid #EEE */
-}
-.tabletitle{
-  //padding: 5px;
-  font-size: .5em;
-  background: #EEE;
-  
-}
-.service{
-  border-bottom: 1px solid #EEE;}
-.item{width: 24mm;}
-.itemtext{font-size: .5em;}
+  }
+  .tabletitle {
+    //padding: 5px;
+    font-size: 0.5em;
+    background: #eee;
+  }
+  .service {
+    border-bottom: 1px solid #eee;
+  }
+  .item {
+    width: 24mm;
+  }
+  .itemtext {
+    font-size: 0.5em;
+  }
 
-#legalcopy{
-  margin-top: 5mm;
-}
-
-  
-  
+  #legalcopy {
+    margin-top: 5mm;
+  }
 }
 </style>
