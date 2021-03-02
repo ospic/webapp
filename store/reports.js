7d403c1bfaa0d852ddab90e5602ade9c94ecaf15
Elirehema <rocksandystone@gmail.com>
2021-03-02 15:17:06 +0300

    Create store for reports

---
+++ store/reports.js
@@ -0,0 +1,52 @@
+
+const state = () => ({
+  showLoader: Boolean,
+  report: {},
+  reports: []
+});
+
+const mutations = {
+
+  ["GET_REPORTS"](state) {
+    state.showLoader = true;
+  },
+  ["GET_REPORTS_FAILED"](state) {
+    state.showLoader = false;
+  },
+  ["GET_REPORTS_ERROR"](state) {
+    state.showLoader = false;
+  },
+  ["GET_REPORTS_SUCCESS"](state, payload) {
+    state.showLoader = false;
+    this.report = payload;
+  },
+
+}
+const actions = {
+
+  async get_reports({ commit }) {
+    commit("GET_REPORTS");
+    await this.$api.$get('reports/')
+      .then(response => {
+        commit("GET_REPORTS_SUCCESS", response);
+
+      }).catch(error => {
+        commit("GET_REPORTS_ERROR");
+        console.log(error);
+
+      });
+
+  }
+
+}
+const getters = {
+
+}
+
+export default {
+  namespaced: false,
+  state,
+  mutations,
+  actions,
+  getters
+}
