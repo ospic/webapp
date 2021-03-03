91a9ff8dd745b50524a21b067af263a3bca32526
Elirehema <rocksandystone@gmail.com>
2021-03-03 23:44:53 +0300

    Configure manual report upload

--- pages/reports/index.vue
+++ pages/reports/index.vue
@@ -26,20 +26,70 @@
           <v-toolbar flat class="default">
             <p class="title">Reports</p>
             <v-spacer></v-spacer>
-            <v-row no-gutters>
-              <v-col cols="12" md="2" align-self="center"> </v-col>
-              <v-col cols="12" md="4">
-                <v-text-field
-                  v-model="search"
-                  append-icon="search"
-                  label="Search by Name/ID ..."
-                  outlined
-                  rounded
-                  single-line
-                  hide-details
-                ></v-text-field>
-              </v-col>
-            </v-row>
+
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Search by Name/ID ..."
+              outlined
+              dense
+              rounded
+              single-line
+              hide-details
+            ></v-text-field>
+
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog" width="500">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="primary"
+                  v-bind="attrs"
+                  v-on="on"
+                  v-if="isMdAndUp"
+                >
+                  <v-icon small>mdi-file-upload</v-icon>&nbsp; Upload report
+                </v-btn>
+                <v-btn
+                  v-else
+                  color="primary"
+                  fab
+                  small
+                  class="mb-2 font-weight-normal"
+                  v-bind="attrs"
+                  v-on="on"
+                  dark
+                  ><v-icon>mdi-file-upload</v-icon>
+                </v-btn>
+              </template>
+
+              <v-card>
+                <v-card-title class="headline grey lighten-2">
+                  Upload new report ?
+                </v-card-title>
+
+                <v-card-text>
+                  <v-file-input
+                    show-size
+                    small-chips
+                    accept=".jrxml"
+                    truncate-length="50"
+                    @change="filechanged"
+                  ></v-file-input>
+                </v-card-text>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary" small @click="dialog = false">
+                    No Deny
+                  </v-btn>
+                  <v-btn color="warning" small @click="uploadreport">
+                    Yes Upload
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
           </v-toolbar>
         </template>
       </v-data-table>
@@ -54,6 +104,8 @@ export default {
       response_url: null,
       response_data: null,
       search: "",
+      dialog: false,
+      currentFile: undefined,
       publicPath: process.env.BASE_URL,
       headers: [
         { text: "ID", value: "id" },
@@ -64,6 +116,17 @@ export default {
   methods: {
     handle_row_click(i) {
       this.$router.push("/reports/" + i.filename);
+    },
+    filechanged: function(file) {
+      this.progress = 0;
+      this.currentFile = file;
+    },
+    uploadreport: function() {
+      this.dialog = false;
+      const formData = new FormData();
+      formData.append("file", this.currentFile);
+      this.$store.dispatch("uploadreport", formData);
+      this.$store.dispatch("get_reports");
     }
   },
   mounted() {
