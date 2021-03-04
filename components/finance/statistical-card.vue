e643908fe4ed6b52bcb15b761d63345f78917f3f
Elirehema <rocksandystone@gmail.com>
2021-03-04 22:40:25 +0300

    Finance statistical bill

--- components/finance/statistical-card.vue
+++ components/finance/statistical-card.vue
@@ -2,21 +2,26 @@
   <v-hover>
     <template v-slot:default="{ hover }">
       <v-card class="default" :elevation="hover ? 3 : 1">
-        <v-list-item>
-          <v-list-item-avatar height="45" width="45" rounded>
-            <v-icon color="primary" size="50" v-text="item.icon"></v-icon>
-          </v-list-item-avatar>
-
+        <v-list-item three-line>
           <v-list-item-content>
             <v-list-item-title
-              class=" font-weight-black my-1"
+              class="text--caption font-weight-normal my-1"
               v-html="item.title"
             ></v-list-item-title>
+
             <v-list-item-subtitle
-              class="text-h5 font-weight-normal"
+              class="text-h5 font-weight-black black--text my-2"
               v-html="item.value"
             ></v-list-item-subtitle>
+
+            <v-list-item-subtitle
+              class="font-weight-normal"
+              v-html="item.subtitle"
+            ></v-list-item-subtitle>
           </v-list-item-content>
+          <v-list-item-avatar height="45" width="45" rounded>
+            <v-icon :color="item.color" size="50" v-text="item.icon"></v-icon>
+          </v-list-item-avatar>
         </v-list-item>
       </v-card>
     </template>
