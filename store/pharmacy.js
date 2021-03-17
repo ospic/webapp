be688a52cc06349e7973aa81ced4f88c080543f5
Elirehema <rocksandystone@gmail.com>
2021-03-17 23:46:34 +0300

    Free pharmacy from mutation imports

--- store/pharmacy.js
+++ store/pharmacy.js
@@ -1,4 +1,3 @@
-import * as mutation from './mutation-types';
 const state = () => ({
   showLoader: Boolean,
   medicine: {},
@@ -10,146 +9,146 @@ const state = () => ({
 
 const mutations = {
   /** Get medicine's  products */
-  [mutation.GET_MEDICINE_PRODUCTS](state) {
+  ["GET_MEDICINE_PRODUCTS"](state) {
     state.showLoader = true;
   },
-  [mutation.GET_MEDICINE_PRODUCTS_FAILED](state) {
+  ["GET_MEDICINE_PRODUCTS_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.GET_MEDICINE_PRODUCTS_ERROR](state) {
+  ["GET_MEDICINE_PRODUCTS_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.GET_MEDICINE_PRODUCTS_SUCCESS](state, payload) {
+  ["GET_MEDICINE_PRODUCTS_SUCCESS"](state, payload) {
     state.showLoader = false;
     state.medicines = payload.body;
   },
 
 
   /**Create medicine products */
-  [mutation.CREATE_NEW_MEDICINE_PRODUCT](state) {
+  ["CREATE_NEW_MEDICINE_PRODUCT"](state) {
     state.showLoader = true;
   },
-  [mutation.CREATE_NEW_MEDICINE_PRODUCT_FAILED](state) {
+  ["CREATE_NEW_MEDICINE_PRODUCT_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.CREATE_NEW_MEDICINE_PRODUCT_ERROR](state) {
+  ["CREATE_NEW_MEDICINE_PRODUCT_ERROR"(state) {
     state.showLoader = false;
   },
-  [mutation.CREATE_NEW_MEDICINE_PRODUCT_SUCCESS](state, payload) {
+  ["CREATE_NEW_MEDICINE_PRODUCT_SUCCESS"](state, payload) {
     state.showLoader = false;
   },
 
   /**Update medicne product */
-  [mutation.UPDATE_MEDICINE_PRODUCT](state) {
+  ["UPDATE_MEDICINE_PRODUCT"](state) {
     state.showLoader = true;
   },
-  [mutation.UPDATE_MEDICINE_PRODUCT_FAILED](state) {
+  ["UPDATE_MEDICINE_PRODUCT_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.UPDATE_MEDICINE_PRODUCT_ERROR](state) {
+  ["UPDATE_MEDICINE_PRODUCT_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.UPDATE_MEDICINE_PRODUCT_SUCCESS](state, payload) {
+  ["UPDATE_MEDICINE_PRODUCT_SUCCESS"](state, payload) {
     state.showLoader = false;
   },
 
   /** Medicine groups */
-  [mutation.FETCH_MEDICINE_GROUPS](state) {
+  ["FETCH_MEDICINE_GROUPS"](state) {
     state.showLoader = true;
   },
-  [mutation.FETCH_MEDICINE_GROUPS_FAILED](state) {
+  ["FETCH_MEDICINE_GROUPS_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.FETCH_MEDICINE_GROUPS_ERROR](state) {
+  ["FETCH_MEDICINE_GROUPS_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.FETCH_MEDICINE_GROUPS_SUCCESS](state, payload) {
+  ["FETCH_MEDICINE_GROUPS_SUCCESS"](state, payload) {
     state.showLoader = false;
     state.medicinegroups = payload;
   },
 
   /** UPDATE Medicine group */
-  [mutation.UPDATE_MEDICINE_GROUP](state) {
+  ["UPDATE_MEDICINE_GROUP"](state) {
     state.showLoader = true;
   },
-  [mutation.UPDATE_MEDICINE_GROUP_FAILED](state) {
+  ["UPDATE_MEDICINE_GROUP_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.UPDATE_MEDICINE_GROUP_ERROR](state) {
+  ["UPDATE_MEDICINE_GROUP_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.UPDATE_MEDICINE_GROUP_SUCCESS](state, payload) {
+  ["UPDATE_MEDICINE_GROUP_SUCCESS"](state, payload) {
     state.showLoader = false;
   },
 
 
   /** Medicine categories */
-  [mutation.FETCH_MEDICINE_CATEGORIES](state) {
+  ["FETCH_MEDICINE_CATEGORIES"](state) {
     state.showLoader = true;
   },
-  [mutation.FETCH_MEDICINE_CATEGORIES_FAILED](state) {
+  ["FETCH_MEDICINE_CATEGORIES_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.FETCH_MEDICINE_CATEGORIES_ERROR](state) {
+  ["FETCH_MEDICINE_CATEGORIES_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.FETCH_MEDICINE_CATEGORIES_SUCCESS](state, payload) {
+  ["FETCH_MEDICINE_CATEGORIES_SUCCESS"](state, payload) {
     state.showLoader = false;
     state.medicinecategories = payload;
   },
 
   /** Create medicine categories */
-  [mutation.CREATE_MEDICINE_CATEGORY](state) {
+  ["CREATE_MEDICINE_CATEGORY"](state) {
     state.showLoader = true;
   },
-  [mutation.CREATE_MEDICINE_CATEGORY_FAILED](state) {
+  ["CREATE_MEDICINE_CATEGORY_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.CREATE_MEDICINE_CATEGORY_ERROR](state) {
+  ["CREATE_MEDICINE_CATEGORY_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.CREATE_MEDICINE_CATEGORY_SUCCESS](state, payload) {
+  ["CREATE_MEDICINE_CATEGORY_SUCCESS"](state, payload) {
     state.showLoader = false;
   },
 
   /** UPDATE medicine categories */
-  [mutation.UPDATE_MEDICINE_CATEGORY](state) {
+  ["UPDATE_MEDICINE_CATEGORY"](state) {
     state.showLoader = true;
   },
-  [mutation.UPDATE_MEDICINE_CATEGORY_FAILED](state) {
+  ["UPDATE_MEDICINE_CATEGORY_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.UPDATE_MEDICINE_CATEGORY_ERROR](state) {
+  ["UPDATE_MEDICINE_CATEGORY_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.UPDATE_MEDICINE_CATEGORY_SUCCESS](state, payload) {
+  ["UPDATE_MEDICINE_CATEGORY_SUCCESS"](state, payload) {
     state.showLoader = false;
   },
   /** Create medicine group */
-  [mutation.CREATE_MEDICINE_GROUP](state) {
+  ["CREATE_MEDICINE_GROUP"](state) {
     state.showLoader = true;
   },
-  [mutation.CREATE_MEDICINE_GROUP_FAILED](state) {
+  ["CREATE_MEDICINE_GROUP_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.CREATE_MEDICINE_GROUP_ERROR](state) {
+  ["CREATE_MEDICINE_GROUP_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.CREATE_MEDICINE_GROUP_SUCCESS](state, payload) {
+  ["CREATE_MEDICINE_GROUP_SUCCESS"](state, payload) {
     state.showLoader = false;
   },
 
   /** Fetch medicine measurement units */
-  [mutation.GET_MEDICINE_MEASURES](state) {
+  ["GET_MEDICINE_MEASURES"](state) {
     state.showLoader = true;
   },
-  [mutation.GET_MEDICINE_MEASURES_FAILED](state) {
+  ["GET_MEDICINE_MEASURES_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.GET_MEDICINE_MEASURES_ERROR](state) {
+  ["GET_MEDICINE_MEASURES_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.GET_MEDICINE_MEASURES_SUCCESS](state, payload) {
+  ["GET_MEDICINE_MEASURES_SUCCESS"](state, payload) {
     state.showLoader = false;
     state.medicinemeasurements = payload;
   },
@@ -185,122 +184,122 @@ const mutations = {
 const actions = {
 
   async getmedicines({ commit }) {
-    commit(mutation.GET_MEDICINE_PRODUCTS);
+    commit("GET_MEDICINE_PRODUCTS");
     await this.$api.$get(`pharmacy/medicines/`)
       .then(response => {
-        commit(mutation.GET_MEDICINE_PRODUCTS_SUCCESS, response);
+        commit("GET_MEDICINE_PRODUCTS_SUCCESS", response);
 
       }).catch(error => {
-        commit(mutation.GET_MEDICINE_PRODUCTS_ERROR);
+        commit("GET_MEDICINE_PRODUCTS_ERROR");
         console.log(error);
 
       });
   },
   async add_new_medicine({ commit }, payload) {
-    commit(mutation.CREATE_NEW_MEDICINE_PRODUCT);
+    commit("CREATE_NEW_MEDICINE_PRODUCT");
     await this.$api.$post(`pharmacy/medicines/`, payload)
       .then(response => {
-        commit(mutation.CREATE_NEW_MEDICINE_PRODUCT_SUCCESS, response);
+        commit("CREATE_NEW_MEDICINE_PRODUCT_SUCCESS", response);
 
       }).catch(error => {
-        commit(mutation.CREATE_NEW_MEDICINE_PRODUCT_ERROR);
+        commit("CREATE_NEW_MEDICINE_PRODUCT_ERROR");
         console.log(error);
 
       });
   },
   async add_new_medicine_category({ commit }, payload) {
-    commit(mutation.CREATE_MEDICINE_CATEGORY);
+    commit("CREATE_MEDICINE_CATEGORY");
     await this.$api.$post(`pharmacy/medicines/categories/`, payload)
       .then(response => {
-        commit(mutation.CREATE_MEDICINE_CATEGORY_SUCCESS, response);
+        commit("CREATE_MEDICINE_CATEGORY_SUCCESS", response);
 
       }).catch(error => {
-        commit(mutation.CREATE_MEDICINE_CATEGORY_ERROR);
+        commit("CREATE_MEDICINE_CATEGORY_ERROR");
         console.log(error);
 
       });
   },
   async add_new_medicine_group({ commit }, payload) {
-    commit(mutation.CREATE_MEDICINE_GROUP);
+    commit("CREATE_MEDICINE_GROUP");
     await this.$api.$post(`pharmacy/medicines/groups/`, payload)
       .then(response => {
-        commit(mutation.CREATE_MEDICINE_GROUP_SUCCESS, response);
+        commit("CREATE_MEDICINE_GROUP_SUCCESS", response);
 
       }).catch(error => {
-        commit(mutation.CREATE_MEDICINE_GROUP_ERROR);
+        commit("CREATE_MEDICINE_GROUP_ERROR");
         console.log(error);
 
       });
   },
   async update_medicine_product({ commit }, payload) {
-    commit(mutation.UPDATE_MEDICINE_PRODUCT);
+    commit("UPDATE_MEDICINE_PRODUCT");
     await this.$api.$put(`pharmacy/medicines/${payload.id}`, payload)
       .then(response => {
-        commit(mutation.UPDATE_MEDICINE_PRODUCT_SUCCESS, response);
+        commit("UPDATE_MEDICINE_PRODUCT_SUCCESS", response);
 
       }).catch(error => {
-        commit(mutation.UPDATE_MEDICINE_PRODUCT_ERROR);
+        commit("UPDATE_MEDICINE_PRODUCT_ERROR");
         console.log(error);
 
       });
   },
   async getmedicinesgroups({ commit }) {
-    commit(mutation.FETCH_MEDICINE_GROUPS);
+    commit("FETCH_MEDICINE_GROUPS");
     await this.$api.$get(`pharmacy/medicines/groups/`)
       .then(response => {
-        commit(mutation.FETCH_MEDICINE_GROUPS_SUCCESS, response);
+        commit("FETCH_MEDICINE_GROUPS_SUCCESS", response);
 
       }).catch(error => {
-        commit(mutation.FETCH_MEDICINE_GROUPS_ERROR);
+        commit("FETCH_MEDICINE_GROUPS_ERROR");
         console.log(error);
 
       });
   },
   async getmedicinescategories({ commit }) {
-    commit(mutation.FETCH_MEDICINE_CATEGORIES);
+    commit("FETCH_MEDICINE_CATEGORIES");
     await this.$api.$get(`pharmacy/medicines/categories/`)
       .then(response => {
-        commit(mutation.FETCH_MEDICINE_CATEGORIES_SUCCESS, response);
+        commit("FETCH_MEDICINE_CATEGORIES_SUCCESS", response);
 
       }).catch(error => {
-        commit(mutation.FETCH_MEDICINE_CATEGORIES_ERROR);
+        commit("FETCH_MEDICINE_CATEGORIES_ERROR");
         console.log(error);
 
       });
   },
 
   async update_medicine_group({ commit }, payload) {
-    commit(mutation.UPDATE_MEDICINE_GROUP);
+    commit("UPDATE_MEDICINE_GROUP");
     await this.$api.$put(`pharmacy/medicines/groups/${payload.id}`, payload)
       .then(response => {
-        commit(mutation.UPDATE_MEDICINE_GROUP_SUCCESS, response);
+        commit("UPDATE_MEDICINE_GROUP_SUCCESS", response);
 
       }).catch(error => {
-        commit(mutation.UPDATE_MEDICINE_GROUP_ERROR);
+        commit("UPDATE_MEDICINE_GROUP_ERROR");
         console.log(error);
 
       });
   },
   async update_medicine_category({ commit }, payload) {
-    commit(mutation.UPDATE_MEDICINE_CATEGORY);
+    commit("UPDATE_MEDICINE_CATEGORY");
     await this.$api.$put(`pharmacy/medicines/categories/${payload.id}`, payload)
       .then(response => {
-        commit(mutation.UPDATE_MEDICINE_CATEGORY_SUCCESS, response);
+        commit("UPDATE_MEDICINE_CATEGORY_SUCCESS", response);
 
       }).catch(error => {
-        commit(mutation.UPDATE_MEDICINE_CATEGORY_ERROR);
+        commit("UPDATE_MEDICINE_CATEGORY_ERROR");
         console.log(error);
 
       });
   },
   async fetch_medicine_measurements({ commit }) {
-    commit(mutation.GET_MEDICINE_MEASURES);
+    commit("GET_MEDICINE_MEASURES");
     await this.$api.$get(`pharmacy/measures/`)
       .then(response => {
-        commit(mutation.GET_MEDICINE_MEASURES_SUCCESS, response);
+        commit("GET_MEDICINE_MEASURES_SUCCES", response);
 
       }).catch(error => {
-        commit(mutation.GET_MEDICINE_MEASURES_ERROR);
+        commit("GET_MEDICINE_MEASURES_ERROR");
         console.log(error);
 
       });
