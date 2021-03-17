f8a5c75f71eae2e1474c2ee9a925a260400395e6
Elirehema <rocksandystone@gmail.com>
2021-03-18 00:08:42 +0300

    free users store from mutation imports

--- store/users.js
+++ store/users.js
@@ -1,4 +1,3 @@
-import * as mutation from './mutation-types';
 const state = () => ({
   showLoader: Boolean,
   user: {},
@@ -8,70 +7,70 @@ const state = () => ({
 });
 
 const mutations = {
-  [mutation.FETCH_APPLICATION_USERS](state) {
+  ["FETCH_APPLICATION_USERS"](state) {
     state.showLoader = true;
   },
-  [mutation.FETCH_APPLICATION_USERS_FAILED](state) {
+  ["FETCH_APPLICATION_USERS_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.FETCH_APPLICATION_USERS_ERROR](state) {
+  ["FETCH_APPLICATION_USERS_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.FETCH_APPLICATION_USERS_SUCCESS](state, payload) {
+  ["FETCH_APPLICATION_USERS_SUCCESS"](state, payload) {
     state.showLoader = false;
     state.users = payload;
   },
-  [mutation.CREATE_NEW_USER](state) {
+  ["CREATE_NEW_USER"](state) {
     state.showLoader = true;
   },
-  [mutation.CREATE_NEW_USER_FAILED](state) {
+  ["CREATE_NEW_USER_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.CREATE_NEW_USER_ERROR](state) {
+  ["CREATE_NEW_USER_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.CREATE_NEW_USER_SUCCESS](state, payload) {
+  ["CREATE_NEW_USER_SUCCESS"](state, payload) {
     state.showLoader = false;
     state.users.push(payload)
   },
-  [mutation.FETCH_STAFFS](state) {
+  ["FETCH_STAFFS"](state) {
     state.showLoader = true;
   },
-  [mutation.FETCH_STAFFS_FAILED](state) {
+  ["FETCH_STAFFS_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.FETCH_STAFFS_ERROR](state) {
+  ["FETCH_STAFFS_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.FETCH_STAFFS_SUCCESS](state, payload) {
+  ["FETCH_STAFFS_SUCCESS"](state, payload) {
     state.showLoader = false;
     state.staffs = payload
   },
 
-  [mutation.UPDATE_STAFF](state) {
+  ["UPDATE_STAFF"](state) {
     state.showLoader = true;
   },
-  [mutation.UPDATE_STAFF_FAILED](state) {
+  ["UPDATE_STAFF_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.UPDATE_STAFF_ERROR](state) {
+  ["UPDATE_STAFF_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.UPDATE_STAFF_SUCCESS](state, payload) {
+  ["UPDATE_STAFF_SUCCESS"](state, payload) {
     state.showLoader = false;
   },
 
   /** Asign staff to department */
-  [mutation.ASSIGN_STAFF_TO_DEPARTMENT](state) {
+  ["ASSIGN_STAFF_TO_DEPARTMENT"](state) {
     state.showLoader = true;
   },
-  [mutation.ASSIGN_STAFF_TO_DEPARTMENT_FAILED](state) {
+  ["ASSIGN_STAFF_TO_DEPARTMENT_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.ASSIGN_STAFF_TO_DEPARTMENT_ERROR](state) {
+  ["ASSIGN_STAFF_TO_DEPARTMENT_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.ASSIGN_STAFF_TO_DEPARTMENT_SUCCESS](state, payload) {
+  ["ASSIGN_STAFF_TO_DEPARTMENT_SUCCESS"](state, payload) {
     state.showLoader = false;
   },
 
@@ -118,14 +117,12 @@ const mutations = {
 }
 const actions = {
   async retrieveAllusers({ commit }) {
-    commit(mutation.FETCH_APPLICATION_USERS);
+    commit("FETCH_APPLICATION_USERS");
     await this.$api.$get(`auth/users/?command=users`)
       .then(response => {
-        commit(mutation.FETCH_APPLICATION_USERS_SUCCESS, response);
-
-
+        commit("FETCH_APPLICATION_USERS_SUCCESS", response);
       }).catch(error => {
-        commit(mutation.FETCH_APPLICATION_USERS_FAILED);
+        commit("FETCH_APPLICATION_USERS_FAILED");
         console.log(error);
 
       });
@@ -136,12 +133,9 @@ const actions = {
     await this.$api.$get(`auth/users/?command=self`)
       .then(response => {
         commit("GET_SELF_SERVICE_USERS_SUCCESS", response);
-
-
       }).catch(error => {
         commit("GET_SELF_SERVICE_USERS_FAILED");
         console.log(error);
-
       });
 
   },
@@ -150,7 +144,6 @@ const actions = {
     await this.$api.$post(`auth/self/`, payload)
       .then(response => {
         commit("CREATE_SERVICE_USERS_SUCCESS", response);
-
       }).catch(error => {
         commit("CREATE_SERVICE_USERS_FAILED");
         console.log(error);
@@ -159,16 +152,16 @@ const actions = {
 
   },
   async create_new_user({ commit }, payload) {
-    commit(mutation.CREATE_NEW_USER);
+    commit("CREATE_NEW_USER");
     await this.$api.$post(`auth/signup/`, payload)
       .then(response => {
         console.log(response);
         if (response.statusCode === 200) {
-          commit(mutation.CREATE_NEW_USER_SUCCESS, response);
+          commit("CREATE_NEW_USER_SUCCESS", response);
         }
 
       }).catch(error => {
-        commit(mutation.CREATE_NEW_USER_FAILED);
+        commit("CREATE_NEW_USER_FAILED");
         console.log(error);
 
       });
@@ -186,48 +179,47 @@ const actions = {
       }).catch(error => {
         commit("UPDATE_USER_FAILED");
         console.log(error);
-
       });
 
   },
   async fetchAllStaffs({ commit }) {
-    commit(mutation.FETCH_STAFFS);
+    commit("FETCH_STAFFS");
     await this.$api.$get(`staffs/status?status=activeavailable`)
       .then(response => {
         if (response != null) {
-          commit(mutation.FETCH_STAFFS_SUCCESS, response);
+          commit("FETCH_STAFFS_SUCCESS", response);
         }
 
       }).catch(error => {
-        commit(mutation.FETCH_STAFFS_FAILED);
+        commit("FETCH_STAFFS_FAILED");
         console.log(error);
 
       });
   },
   async updatestaff({ commit }, payload) {
-    commit(mutation.UPDATE_STAFF);
+    commit("UPDATE_STAFF");
     await this.$api.$put(`staffs/${payload.id}`, payload)
       .then(response => {
         if (response != null) {
-          commit(mutation.UPDATE_STAFF_SUCCESS, response);
+          commit("UPDATE_STAFF_SUCCESS", response);
         }
 
       }).catch(error => {
-        commit(mutation.UPDATE_STAFF_ERROR);
+        commit("UPDATE_STAFF_ERROR");
         console.log(error);
 
       });
   },
   async assign_staff_to_departemnt({ commit }, payload) {
-    commit(mutation.ASSIGN_STAFF_TO_DEPARTMENT);
+    commit("ASSIGN_STAFF_TO_DEPARTMENT");
     await this.$api.$post(`staffs/assign`, payload)
       .then(response => {
         if (response != null) {
-          commit(mutation.ASSIGN_STAFF_TO_DEPARTMENT_SUCCESS, response);
+          commit("ASSIGN_STAFF_TO_DEPARTMENT_SUCCESS", response);
         }
 
       }).catch(error => {
-        commit(mutation.ASSIGN_STAFF_TO_DEPARTMENT_ERROR);
+        commit("ASSIGN_STAFF_TO_DEPARTMENT_ERROR");
         console.log(error);
 
       });
