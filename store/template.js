6079c09fbb1f98638b813681e22e412a9b2383f4
Elirehema <rocksandystone@gmail.com>
2021-03-18 00:04:22 +0300

    Free templates store from mutation imports

--- store/template.js
+++ store/template.js
@@ -1,4 +1,3 @@
-import * as mutation from './mutation-types';
 const state = () => ({
   showLoader: Boolean,
   physician: {},
@@ -9,29 +8,29 @@ const state = () => ({
 
 const mutations = {
 
-  [mutation.GET_PATIENT_CREATION_TEMPLATE](state) {
+  ["GET_PATIENT_CREATION_TEMPLATE"](state) {
     state.showLoader = true;
   },
-  [mutation.GET_PATIENT_CREATION_TEMPLATE_FAILED](state) {
+  ["GET_PATIENT_CREATION_TEMPLATE_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.GET_PATIENT_CREATION_TEMPLATE_ERROR](state) {
+  ["GET_PATIENT_CREATION_TEMPLATE_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.GET_PATIENT_CREATION_TEMPLATE_SUCCESS](state, payload) {
+  ["GET_PATIENT_CREATION_TEMPLATE_SUCCESS"](state, payload) {
     state.showLoader = false;
     state.physicians = payload.physicianOptions;
   },
-  [mutation.MEDICINE_TEMPLATE](state) {
+  ["MEDICINE_TEMPLATE"](state) {
     state.showLoader = true;
   },
-  [mutation.MEDICINE_TEMPLATE_FAILED](state) {
+  ["MEDICINE_TEMPLATE_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.MEDICINE_TEMPLATE_ERROR](state) {
+  ["MEDICINE_TEMPLATE_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.MEDICINE_TEMPLATE_SUCCESS](state, payload) {
+  ["MEDICINE_TEMPLATE_SUCCESS"](state, payload) {
     state.showLoader = false;
     state.medicine_template = payload;
   },
@@ -39,25 +38,25 @@ const mutations = {
 }
 const actions = {
   async retrievephysicians({ commit }) {
-    commit(mutation.GET_PATIENT_CREATION_TEMPLATE);
+    commit("GET_PATIENT_CREATION_TEMPLATE");
     await this.$api.$get(`patients/?command=template`)
       .then(response => {
-        commit(mutation.GET_PATIENT_CREATION_TEMPLATE_SUCCESS, response);
+        commit("GET_PATIENT_CREATION_TEMPLATE_SUCCESS", response);
 
       }).catch(error => {
-        commit(mutation.GET_PATIENT_CREATION_TEMPLATE_ERROR);
+        commit("GET_PATIENT_CREATION_TEMPLATE_ERROR");
         console.log(error);
 
       });
 
   },
   async retrieve_medicine_template({ commit }) {
-    commit(mutation.MEDICINE_TEMPLATE);
+    commit("MEDICINE_TEMPLATE");
     await this.$api.$get(`pharmacy/medicines/template/`)
       .then(response => {
-        commit(mutation.MEDICINE_TEMPLATE_SUCCESS, response);
+        commit("MEDICINE_TEMPLATE_SUCCESS", response);
       }).catch(error => {
-        commit(mutation.MEDICINE_TEMPLATE_FAILED);
+        commit("MEDICINE_TEMPLATE_FAILED");
         console.log(error);
 
       });
