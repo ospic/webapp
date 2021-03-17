d30791c43fdfb1f1fe943543626f7a8ddbd04b61
Elirehema <rocksandystone@gmail.com>
2021-03-17 23:53:22 +0300

    free patient profile from mutation imports

--- store/profile.js
+++ store/profile.js
@@ -1,4 +1,3 @@
-import * as mutation from './mutation-types';
 const state = () => ({
   showLoader: Boolean,
   profile: {}
@@ -6,31 +5,31 @@ const state = () => ({
 
 const mutations = {
 
-  [mutation.GET_PROFILE](state) {
+  ["GET_PROFILE"](state) {
     state.showLoader = true;
   },
-  [mutation.GET_PROFILE_FAILED](state) {
+  ["GET_PROFILE_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.GET_PROFILE_ERROR](state) {
+  ["GET_PROFILE_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.GET_PROFILE_SUCCESS](state, payload) {
+  ["GET_PROFILE_SUCCESS"](state, payload) {
     state.showLoader = false;
     state.profile = payload;
 
   },
 
-  [mutation.UPDATE_PASSWORD](state) {
+  ["UPDATE_PASSWORD"](state) {
     state.showLoader = true;
   },
-  [mutation.UPDATE_PASSWORD_FAILED](state) {
+  ["UPDATE_PASSWORD_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.UPDATE_PASSWORD_ERROR](state) {
+  ["UPDATE_PASSWORD_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.UPDATE_PASSWORD_SUCCESS](state, payload) {
+  ["UPDATE_PASSWORD_SUCCESS"](state, payload) {
     state.showLoader = false;
 
   },
@@ -38,30 +37,30 @@ const mutations = {
 }
 const actions = {
   async retrieve_profile({ commit }) {
-    commit(mutation.GET_PROFILE);
+    commit("GET_PROFILE");
     await this.$api.$get("auth/users/me")
       .then(response => {
-        commit(mutation.GET_PROFILE_SUCCESS, response);
+        commit("GET_PROFILE_SUCCESS", response);
 
 
       }).catch(error => {
-        commit(mutation.GET_PROFILE_ERROR);
+        commit("GET_PROFILE_ERROR");
         console.log(error);
 
       });
 
   },
   async _update_user_password({ commit }, payload) {
-    commit(mutation.UPDATE_PASSWORD);
+    commit("UPDATE_PASSWORD");
     await this.$api.$post("auth/password", payload)
       .then(response => {
-        commit(mutation.UPDATE_PASSWORD_SUCCESS, response);
+        commit("UPDATE_PASSWORD_SUCCESS", response);
         if (response.httpStatus === 'OK') {
-          commit(mutation.SIGNOUT_SUCCESS)
+          commit("SIGNOUT_SUCCESS")
         }
 
       }).catch(error => {
-        commit(mutation.UPDATE_PASSWORD_ERROR);
+        commit("UPDATE_PASSWORD_ERROR");
         console.log(error);
 
       });
