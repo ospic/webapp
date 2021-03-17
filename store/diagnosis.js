489294545f02ccf8fcd6f443c3af63a8b1e8a8f5
Elirehema <rocksandystone@gmail.com>
2021-03-17 23:27:08 +0300

    free diagnoses file from mutations

--- store/diagnosis.js
+++ store/diagnosis.js
@@ -1,4 +1,3 @@
-import * as mutation from './mutation-types';
 const state = () => ({
   showLoader: Boolean,
   diagnosis: {},
@@ -6,16 +5,16 @@ const state = () => ({
 });
 
 const mutations = {
-  [mutation.ADD_NEW_DIAGNOSIS](state) {
+  ["ADD_NEW_DIAGNOSIS"](state) {
     state.showLoader = true;
   },
-  [mutation.ADD_NEW_DIAGNOSIS_FAILED](state) {
+  ["ADD_NEW_DIAGNOSIS_FAILED"](state) {
     state.showLoader = false;
   },
-  [mutation.ADD_NEW_DIAGNOSIS_ERROR](state) {
+  ["ADD_NEW_DIAGNOSIS_ERROR"](state) {
     state.showLoader = false;
   },
-  [mutation.ADD_NEW_DIAGNOSIS_SUCCESS](state, payload) {
+  ["ADD_NEW_DIAGNOSIS_SUCCESS"](state, payload) {
     state.showLoader = false;
     state.diagnosis = payload;
 
@@ -24,13 +23,13 @@ const mutations = {
 
 const actions = {
   async create_new_diagnosis({ commit }, id, payload) {
-    commit(mutation.ADD_NEW_DIAGNOSIS);
+    commit("ADD_NEW_DIAGNOSIS");
     await this.$api.$post(`diagnoses/${id}`, payload)
       .then(response => {
-        commit(mutation.ADD_NEW_DIAGNOSIS_SUCCESS, response);
+        commit("ADD_NEW_DIAGNOSIS_SUCCESS", response);
 
       }).catch(error => {
-        commit(mutation.ADD_NEW_DIAGNOSIS_ERROR);
+        commit("ADD_NEW_DIAGNOSIS_ERROR");
         console.log(error);
 
       });
