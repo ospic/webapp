f5a82fdfd7bf51a9312ab9f8276da5b396c533fa
Elirehema <rocksandystone@gmail.com>
2021-03-27 02:34:42 +0300

    Persisit store to vuex

--- plugins/vuepersistence.js
+++ plugins/vuepersistence.js
@@ -8,7 +8,7 @@ export default ({ store }) => {
       paths: [
         "authentication", "inventory", "profile", "diagnoses",
         "templates", "users", "patients", "documents", "statistics", "theme", "smsconfiguration", "statisitics",
-        "consultations", "departments", "medicalservices", "laboratory", 'bills', 'radiology', 'reports'],
+        "consultations", "departments", "medicalservices", "laboratory", 'bills', 'radiology', 'reports', 'accounting'],
       storage: {
         getItem: key => ls.get(key),
         setItem: (key, value) => ls.set(key, value),
