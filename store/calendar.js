5de1c7d2a0e4249f4a10093a680d6fdba3f317b1
Elirehema <rocksandystone@gmail.com>
2021-05-04 19:55:59 +0300

    Create store event class

---
+++ store/calendar.js
@@ -0,0 +1,56 @@
+const state = () => ({
+  showLoader: Boolean,
+  event: {},
+  events: [],
+
+});
+
+const mutations = {
+
+  /**Populate blood bank  */
+  ["FETCH_CALENDAR_EVENTS"](state) {
+    state.showLoader = true;
+  },
+  ["FETCH_CALENDAR_EVENTS_FAILED"](state) {
+    state.showLoader = false;
+  },
+  ["FETCH_CALENDAR_EVENTS_ERROR"](state) {
+    state.showLoader = false;
+  },
+  ["FETCH_CALENDAR_EVENTS_SUCCESS"](state, payload) {
+    state.showLoader = false;
+    state.CALENDAR_EVENTS = payload;
+  },
+
+
+}
+const actions = {
+  async getcalendar_events({ commit }) {
+    commit("FETCH_CALENDAR_EVENTS");
+    await this.$api.$get(`calendar/`)
+      .then(response => {
+        commit("FETCH_CALENDAR_EVENTS_SUCCESS", response);
+
+      }).catch(error => {
+        commit("FETCH_CALENDAR_EVENTS_ERROR");
+        console.log(error);
+
+      });
+  },
+
+}
+
+const getters = {
+  events: function (state) {
+    return state.events;
+  }
+
+}
+
+export default {
+  namespaced: false,
+  state,
+  mutations,
+  actions,
+  getters
+}
