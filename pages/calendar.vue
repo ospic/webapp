4321df343134e8431264b2e80b3ae31f0890e054
Elirehema <rocksandystone@gmail.com>
2021-05-05 13:25:10 +0300

    Create events from interface

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -10,7 +10,179 @@
           <strong>Events</strong>
         </v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn class="button"><v-icon left>mdi-plus</v-icon> new events</v-btn>
+        <v-dialog
+          v-model="dialog"
+          transition="dialog-bottom-transition"
+          max-width="800"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn class="button" v-bind="attrs" v-on="on"
+              ><v-icon left>mdi-plus</v-icon> new events</v-btn
+            >
+          </template>
+          <template v-slot:default="dialog">
+            <v-card>
+              <v-form ref="form" v-model="valid" lazy-validation>
+                <v-toolbar flat color="primary" dark>Add new event</v-toolbar>
+
+                <v-card-text>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-text-field
+                        autofocus
+                        v-model="event.name"
+                        label="Event name"
+                        clearable
+                        outlined
+                        :rules="[v => !!v || 'Field is required']"
+                        required
+                      >
+                      </v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="12" md="6">
+                      <v-menu
+                        ref="menu"
+                        v-model="menu"
+                        :close-on-content-click="false"
+                        :return-value.sync="dates"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-combobox
+                            v-model="dates"
+                            multiple
+                            chips
+                            small-chips
+                            label="Selet event start date and end date"
+                            prepend-icon="mdi-calendar"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                          ></v-combobox>
+                        </template>
+                        <v-date-picker
+                          v-model="dates"
+                          multiple
+                          no-title
+                          scrollable
+                          range
+                          reactive
+                          :min="nowDate"
+                        >
+                          <v-spacer></v-spacer>
+                          <v-btn
+                            text
+                            class="button cancel"
+                            @click="menu = false"
+                          >
+                            Cancel
+                          </v-btn>
+                          <v-btn
+                            text
+                            class="button"
+                            @click="$refs.menu.save(dates)"
+                          >
+                            OK
+                          </v-btn>
+                        </v-date-picker>
+                      </v-menu>
+                    </v-col>
+
+                    <v-col sm="12" md="6">
+                      <v-checkbox
+                        v-model="event.timed"
+                        :label="`Timed: ${event.timed}`"
+                      ></v-checkbox>
+                    </v-col>
+                    <v-col cols="12" sm="12" md="6">
+                      <v-menu
+                        ref="start"
+                        v-model="menu2"
+                        :close-on-content-click="false"
+                        :nudge-right="40"
+                        :return-value.sync="event.startTime"
+                        transition="scale-transition"
+                        offset-y
+                        max-width="290px"
+                        min-width="290px"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="event.startTime"
+                            label="Start time"
+                            prepend-icon="mdi-clock-time-four-outline"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                            :rules="[v => !!v || 'Field is required']"
+                            required
+                          ></v-text-field>
+                        </template>
+                        <v-time-picker
+                          v-if="menu2"
+                          v-model="event.startTime"
+                          full-width
+                          ampm-in-title
+                          @click:minute="$refs.start.save(event.startTime)"
+                        ></v-time-picker>
+                      </v-menu>
+                    </v-col>
+                    <v-spacer></v-spacer>
+
+                    <v-col cols="11" sm="12" md="6">
+                      <v-dialog
+                        ref="dialog"
+                        v-model="modal2"
+                        :return-value.sync="event.endTime"
+                        persistent
+                        width="290px"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="event.endTime"
+                            label="End time"
+                            prepend-icon="mdi-clock-time-four-outline"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                            :rules="[v => !!v || 'Field is required']"
+                            required
+                          ></v-text-field>
+                        </template>
+
+                        <v-time-picker
+                          v-if="modal2"
+                          v-model="event.endTime"
+                          full-width
+                        >
+                          <v-spacer></v-spacer>
+                          <v-btn text color="primary" @click="modal2 = false">
+                            Cancel
+                          </v-btn>
+                          <v-btn
+                            text
+                            color="primary"
+                            @click="$refs.dialog.save(event.endTime)"
+                          >
+                            OK
+                          </v-btn>
+                        </v-time-picker>
+                      </v-dialog>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+                <v-card-actions class="justify-end">
+                  <v-btn @click="dialog.value = false" class="button cancel"
+                    >Cancel</v-btn
+                  >
+                  <v-btn @click="save_event" class="button">Save</v-btn>
+                </v-card-actions>
+              </v-form>
+            </v-card>
+          </template>
+        </v-dialog>
       </v-toolbar>
       <v-card-text>
         <v-row class="fill-height">
@@ -125,7 +297,17 @@ import { mapGetters } from "vuex";
 export default {
   data: () => ({
     focus: "",
+    dates: [],
     type: "month",
+    time: null,
+    menu: false,
+    menu2: false,
+    modal2: false,
+    valid: true,
+    dialog: false,
+    nowDate: new Date().toISOString().slice(0, 10),
+    today: new Date().toISOString(),
+    timeNow: new Date().toTimeString().split(" ")[0],
     typeToLabel: {
       month: "Month",
       week: "Week",
@@ -136,6 +318,15 @@ export default {
     selectedElement: null,
     selectedOpen: false,
     events: [],
+    event: {
+      name: null,
+      startDate: null,
+      startTime: null,
+      endDate: null,
+      endTime: null,
+      timed: true,
+      departmentId: null
+    },
     colors: [
       "blue",
       "indigo",
@@ -161,6 +352,21 @@ export default {
     this.$store.dispatch("get_calendar_events");
   },
   methods: {
+    save_event: function() {
+      this.event.startDate = this.dates[0];
+      this.event.endDate = this.dates[1];
+      if (this.$refs.form.validate()) {
+        this.$store.dispatch("create_calendar_event", this.event).then(() => {
+          setTimeout(
+            () => this.$store.dispatch("get_calendar_events"),
+            this.delay_seconds
+          );
+          setTimeout(() => (this.focus = this.focus + " "), this.delay_seconds);
+        });
+      }
+      this.$refs.form.reset();
+      this.dialog = false;
+    },
     viewDay({ date }) {
       this.focus = date;
       this.type = "day";
